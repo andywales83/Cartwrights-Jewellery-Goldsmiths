c0f4fcb37505f3339a3cf8803db4bb2a008e4707
Andrew Llewellyn <andrew.llewellyn83@gmail.com>
2022-02-05 10:41:44 +0000

    Added home app and templates

---
+++ home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    """ A view that returns the main index page for the user """
+
+    return render(request, 'home/index.html')
