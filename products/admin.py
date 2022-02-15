fdc9ea37db1aee739fa42dcb8e2b647caba30bb1
Andrew Llewellyn <andrew.llewellyn83@gmail.com>
2022-02-15 20:59:46 +0000

    Added products app, models and fixtures

---
+++ products/admin.py
@@ -0,0 +1,7 @@
+"""Product App Admin Classes"""
+from django.contrib import admin
+from .models import Product, Category
+
+# Register your models here.
+admin.site.register(Product)
+admin.site.register(Category)
