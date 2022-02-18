4cf6bad6c46729a78ec92c14b7c8a98f8ecebffd
Andrew Llewellyn <andrew.llewellyn83@gmail.com>
2022-02-18 17:55:04 +0000

    customised Admin

--- products/models.py
+++ products/models.py
@@ -4,6 +4,10 @@ from django.db import models
 
 class Category(models.Model):
     """Creates Genre table in database"""
+    class Meta:
+        """ To ensure the plural of Catergory is correcrly specified. """
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
