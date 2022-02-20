cb22002e3ada15772db8d8e94a3930e3ef827986
Andrew Llewellyn <andrew.llewellyn83@gmail.com>
2022-02-20 14:19:01 +0000

    Added category filtering

--- products/views.py
+++ products/views.py
@@ -6,7 +6,7 @@ to add, edit and delete products.
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -16,8 +16,14 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -32,6 +38,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
