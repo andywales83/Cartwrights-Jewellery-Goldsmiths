0077ab5c4bc3ffbee16261066e6e2ac8fe3d8da6
Andrew Llewellyn <andrew.llewellyn83@gmail.com>
2022-03-31 18:32:19 +0000

    fininshed the add product functionality

--- products/views.py
+++ products/views.py
@@ -1,3 +1,7 @@
+"""
+Views for products, including product detail view,
+to add, edit and delete products.
+"""
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
@@ -73,7 +77,18 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ Add a product to the store """
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please \
+                ensure the form is valid.')
+    else:
+        form = ProductForm()
+
     template = 'products/add_product.html'
     context = {
         'form': form,
