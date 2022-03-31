5eee31e4866f08f88d64b743506297f991391b1f
Andrew Llewellyn <andrew.llewellyn83@gmail.com>
2022-03-31 16:07:04 +0000

    Added order histor and updated checkout views for profiles

--- profiles/views.py
+++ profiles/views.py
@@ -4,14 +4,16 @@ User Profile views
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-
+from checkout.models import Order
 from .models import UserProfile
 from .forms import UserProfileForm
 
 
 @login_required
 def profile(request):
-    """ Display the user's profile. """
+    """
+    Display the user's profile.
+    """
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
@@ -32,6 +34,27 @@ def profile(request):
         'form': form,
         'orders': orders,
         'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def order_history(request, order_number):
+    """
+    Display the user's order history
+    """
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
