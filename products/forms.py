4d487fbbcd9f2141011abb17e790f5da78af8bd2
Andrew Llewellyn <andrew.llewellyn83@gmail.com>
2022-04-01 13:20:33 +0000

    fixing the image field on the product management page pt1

--- products/forms.py
+++ products/forms.py
@@ -2,6 +2,7 @@
 Product Form Classes
 """
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Product, Category
 
 
@@ -15,6 +16,9 @@ class ProductForm(forms.ModelForm):
         """
         model = Product
         fields = '__all__'
+
+    image = forms.ImageField(
+        label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
