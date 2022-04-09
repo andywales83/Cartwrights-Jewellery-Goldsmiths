c92a44b79f9881e94b4a62f79d359699bbc4ed6a
Andrew Llewellyn <andrew.llewellyn83@gmail.com>
2022-04-09 11:01:15 +0000

    Changed url for all reviews to allow for add-review url to work

--- reviews/urls.py
+++ reviews/urls.py
@@ -6,6 +6,6 @@ from . import views
 
 urlpatterns = [
     path('', views.reviews, name='reviews'),
-    path('<slug:slug>/', views.review_details, name='review_details'),
+    path('detail/<slug:slug>/', views.review_details, name='review_details'),
     path('add_review/', views.add_review, name='add_review'),
 ]
