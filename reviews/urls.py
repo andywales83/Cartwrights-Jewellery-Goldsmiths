8f3775fb7c4ce84f447d374a35731f72b076e79b
Andrew Llewellyn <andrew.llewellyn83@gmail.com>
2022-04-09 18:16:16 +0000

    Added functionality to edit and delete User Reviews

--- reviews/urls.py
+++ reviews/urls.py
@@ -6,6 +6,10 @@ from . import views
 
 urlpatterns = [
     path('', views.reviews, name='reviews'),
-    path('detail/<slug:slug>/', views.review_details, name='review_details'),
     path('add_review/', views.add_review, name='add_review'),
+    path('edit_review/<slug:slug>/',
+         views.edit_review, name='edit_review'),
+    path('delete_review/<slug:slug>/',
+         views.delete_review, name='delete_review'),
+    path('<slug:slug>/', views.review_details, name='review_details'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,7 @@
 """
 User review model
 """
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
@@ -58,6 +58,10 @@ def add_review(request):
     """
     Add a new user review
     """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
@@ -76,3 +80,50 @@ def add_review(request):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def edit_review(request, slug):
+    """
+    Edit a product in the store
+    """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    post = get_object_or_404(Post, slug=slug)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('review_detail', args=[post.slug]))
+        else:
+            messages.error(request, 'Failed to update product. Please \
+                ensure the form is valid.')
+    else:
+        form = PostForm(instance=post)
+        messages.info(request, f'You are editing {post.title}')
+
+    template = 'reviews/edit_review.html'
+    context = {
+        'form': form,
+        'post': post,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_review(request, slug):
+    """
+    Delete a product from the store
+    """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    post = get_object_or_404(Post, slug=slug)
+    post.delete()
+    messages.success(request, 'Review has been deleted!')
+    return redirect(reverse('reviews'))
