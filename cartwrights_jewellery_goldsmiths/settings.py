102eaca439f6f6b5f2c1e752b8d7768928a268d6
Andrew Llewellyn <andrew.llewellyn83@gmail.com>
2022-04-10 14:00:41 +0000

    Test2 for user administration - Unable to login as Admin on Heroku

--- cartwrights_jewellery_goldsmiths/settings.py
+++ cartwrights_jewellery_goldsmiths/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '46fz&)%+ofq*kcgis%n09s$4ptpt00b*)ne1571&!3-(y%pmr(')
+SECRET_KEY = ('46fz&)%+ofq*kcgis%n09s$4ptpt00b*)ne1571&!3-(y%pmr(')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
@@ -123,17 +123,12 @@ CSRF_TRUSTED_ORIGINS = ['https://8000-andywales83-cartwrightsj-8pwiyeelt6b.ws-eu
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else: 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 
 # Password validation
