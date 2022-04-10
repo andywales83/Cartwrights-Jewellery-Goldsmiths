75a130a522cbef9380f783ae6d40fb3114eabcbe
Andrew Llewellyn <andrew.llewellyn83@gmail.com>
2022-04-10 14:32:59 +0000

    Amedn secret Key setup in settings

--- cartwrights_jewellery_goldsmiths/settings.py
+++ cartwrights_jewellery_goldsmiths/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ('46fz&)%+ofq*kcgis%n09s$4ptpt00b*)ne1571&!3-(y%pmr(')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
