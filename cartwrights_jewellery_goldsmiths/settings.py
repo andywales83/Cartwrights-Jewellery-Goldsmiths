c05412c7078949c78f1078382e9cc9e942e717c5
Andrew Llewellyn <andrew.llewellyn83@gmail.com>
2022-04-10 14:10:23 +0000

    Rework of the deployment section following issues with superuser logins

--- cartwrights_jewellery_goldsmiths/settings.py
+++ cartwrights_jewellery_goldsmiths/settings.py
@@ -123,12 +123,17 @@ CSRF_TRUSTED_ORIGINS = ['https://8000-andywales83-cartwrightsj-8pwiyeelt6b.ws-eu
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
